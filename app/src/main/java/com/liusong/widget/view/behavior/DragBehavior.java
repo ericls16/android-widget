ad25b247af81a2b85455dc92fbe9d2ed270b1ee0
桃花林里练醉拳 <jsmrliu@gmail.com>
2017-04-25 15:16:53 +0800

    rv

--- app/src/main/java/com/liusong/widget/view/behavior/DragBehavior.java
+++ app/src/main/java/com/liusong/widget/view/behavior/DragBehavior.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 /**
+ * behavior:某个view监听另一个view的状态变化
  * 拖拽行为
  * Created by liusong on 2016/6/1.
  */
@@ -20,24 +21,25 @@ public class DragBehavior extends CoordinatorLayout.Behavior<Button> {
         width = display.widthPixels;
     }
 
+    /**
+     * 确定依赖的View
+     */
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, Button child, View dependency) {
         //如果dependency是TempView的实例，说明它就是我们所需要的Dependency
         return dependency instanceof DragView;
     }
 
-    //每次dependency位置发生变化，都会执行onDependentViewChanged方法
+    /**
+     * 每次dependency位置发生变化，都会执行onDependentViewChanged方法
+     */
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, Button btn, View dependency) {
-
         //根据dependency的位置，设置Button的位置
-
         int top = dependency.getTop();
         int left = dependency.getLeft();
-
         int x = width - left - btn.getWidth();
         int y = top;
-
         setPosition(btn, x, y);
         return true;
     }
